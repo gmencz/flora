caa99e8ddb06ab84ef790d53a15c5206097f1dbe
Gabriel <yo@gabrielmendezc.com>
2021-02-25 02:50:50 +0100

    first commit
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from "next/dist/next-server/lib/router/router";
+import { useRef } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const queryClientRef = useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+
+  return (
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,116 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { useEffect, useRef } from "react";
+import { useQuery, useQueryClient } from "react-query";
+import { SubscriptionClient } from "subscriptions-transport-ws";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+interface CountriesQuery {
+  data: {
+    countries: {
+      id: string;
+      name: string;
+    }[];
+  };
+}
 
-export default IndexPage
+async function fetchCountries(): Promise<CountriesQuery> {
+  const res = await fetch("http://localhost:8080/v1/graphql", {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      operationName: "Countries",
+      query: `
+        query Countries {
+          countries {
+            id
+            name
+          }
+        }
+      `,
+      variables: null,
+    }),
+  });
+
+  const data = await res.json();
+  return data;
+}
+
+const IndexPage = () => {
+  const countriesQuery = useQuery("countries", fetchCountries);
+  const isInitialDataRef = useRef(true);
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    const client = new SubscriptionClient("ws://localhost:8080/v1/graphql", {
+      reconnect: true,
+    });
+
+    client
+      .request({
+        operationName: "Countries",
+        query: `
+          subscription Countries {
+            countries {
+              id
+              name
+            }
+          }
+      `,
+      })
+      .subscribe({
+        next: (data) => {
+          if (!isInitialDataRef.current) {
+            queryClient.setQueryData<CountriesQuery>(
+              "countries",
+              (existingCountries) => {
+                const { data: countries } = data as CountriesQuery;
+                if (!existingCountries) {
+                  return { data: countries };
+                }
+
+                const uniqueCountries = Array.from(
+                  new Set(countries.countries.map((country) => country.id))
+                ).map(
+                  (id) =>
+                    countries.countries.find((country) => country.id === id)!
+                );
+
+                return {
+                  data: {
+                    countries: uniqueCountries,
+                  },
+                };
+              }
+            );
+          }
+
+          isInitialDataRef.current = false;
+        },
+      });
+  }, []);
+
+  if (countriesQuery.isLoading) {
+    return (
+      <div style={{ padding: "1rem 2rem" }}>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (countriesQuery.isError) {
+    return (
+      <div style={{ padding: "1rem 2rem" }}>
+        <p>Oops! Something went wrong</p>
+      </div>
+    );
+  }
+
+  return (
+    <div style={{ padding: "1rem 2rem" }}>
+      <h1>Countries:</h1>
+      <pre>{JSON.stringify(countriesQuery.data?.data.countries, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default IndexPage;
