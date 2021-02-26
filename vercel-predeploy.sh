c7bc7ca070a5599107bd0cf5d4b76741d35cf2e7
Gabriel <yo@gabrielmendezc.com>
2021-02-26 23:53:32 +0100

    Update predeploy script
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- vercel-predeploy.sh
+++ vercel-predeploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [[ "$BRANCH" == "staging" || "$BRANCH" == "main"  ]] ; then
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
   # Proceed with the build
-  echo "✅ - Build can proceed"
+    echo "✅ - Build can proceed"
   exit 1;
 
 else
