c7e5f7b28874920b20795494538d7dedcbbea7c2
Gabriel <yo@gabrielmendezc.com>
2021-02-28 21:07:25 +0100

    Add favicon and meta tags
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,32 @@
-import { AppProps } from "next/dist/next-server/lib/router/router";
-import { useRef } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
-import { Hydrate } from "react-query/hydration";
-import "../styles/globals.css";
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { useRef } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { Hydrate } from 'react-query/hydration'
+import Head from 'next/head'
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const queryClientRef = useRef<QueryClient>();
+  const queryClientRef = useRef<QueryClient>()
   if (!queryClientRef.current) {
-    queryClientRef.current = new QueryClient();
+    queryClientRef.current = new QueryClient()
   }
 
   return (
     <QueryClientProvider client={queryClientRef.current}>
       <Hydrate state={pageProps.dehydratedState}>
+        <Head>
+          <meta charSet="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>Chatskee</title>
+        </Head>
         <Component {...pageProps} />
       </Hydrate>
       <ReactQueryDevtools />
     </QueryClientProvider>
-  );
+  )
 }
